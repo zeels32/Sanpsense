8080310346d3e4fac765ef3aca4c5e4b848493ed
Zeel <solankizeel91@gmail.com>
2026-09-01 20:45:34 +0530

    feat: increment version code to 3 and comment out unused foreground service permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.pixense.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
